1b1b50a5c02d657241e1d5f67ce60bca09db8180
SleepingDaemon <sleepingdaemon@icloud.com>
2024-11-24 10:49:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*  Ideas for the Text Adventure Game (Phase 1: C# Fundamentals. No OOP)
+ *  
+ *  Array of int representing rooms
+ *  Player class will have a name, inventory, and attributes (HP, Attack Points)
+ *  direction for player to choose which room to go next
+ *  a room can contain an empty room, enemy, boss, treasure
+ * 
+ */
+
+
